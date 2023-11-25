cd4928251f48c53dad5f7416b5638b130f75f50f
boris-moduscreate <boris.berenberg@moduscreate.com>
2023-11-24 15:39:20 -1000

    forge local processing is working

--- src/processHar.js
+++ src/processHar.js
@@ -4,6 +4,7 @@ import api, { route } from "@forge/api";
 import FormData from "form-data";
 import { Buffer } from 'buffer'; 
 import { storage } from '@forge/api';
+import { sanitize } from "./har_sanitize"; // Import your sanitize function
 const { createHash } = require('crypto');
 
 
@@ -125,10 +126,9 @@ resolver.define("processHar", async ({ payload, context }) => {
             // TODO - this probably also needs some kind of trigger callback in case scrubbing takes longer
             // than the max function runtime.
 
-
-            // TODO P1 - We're getting booted on limits (https://developers.cloudflare.com/workers/platform/limits/) sometimes 
-            // So this at least needs to be documented and then we need to ideally gracefully fail.
             console.log('scrubbing the file');
+
+            /*
             const response = await api.fetch(`https://har.securely.abrega.com/scrub`, {
                 body: JSON.stringify(options),
                 method: "post",
@@ -137,58 +137,62 @@ resolver.define("processHar", async ({ payload, context }) => {
             
             const responseText = await response.text();
             console.log('Har Cleaner Response Status:', response.status);
+            */
 
-            if (response.ok) {  // Checks if status code is 200-299
-                const sanitizedContent = JSON.parse(responseText);  // Parse the response text
-                console.log('scrubbed the file');
-                // Create a new attachment with the sanitized content
-                const createAttachmentSuccessful = await createAttachment(issueIdOrKey, sanitizedContent, fileName);
-                // Delete the original attachments only if the new attachment was created successfully
-                if (createAttachmentSuccessful.status) {
+            const harData = typeof harObject === 'string' ? harObject : JSON.stringify(harObject);
 
-                    // Dispatch comment cleanup to seperate queue
-                    const payload = {
-                        attachmentId: attachmentId,
-                        issueIdOrKey: issueIdOrKey,
-                        originalAttachmentMediaId: originalAttachmentMediaId,
-                        newAttachmentMediaId: createAttachmentSuccessful.id,
-                    };
+            let scrubbedHarString;
+            try {
+                // TODO: THE IMPORTANT THING IS THAT WE NEED TO FIGURE OUT SOME KIND OF HANDLING HERE FOR TIMEOUTS I GUESS?
+                scrubbedHarString = sanitize(harData, {
+                    scrubWords: options.words,
+                    scrubMimetypes: options.mime_types,
+                    allCookies: options.all_cookies,
+                    allHeaders: options.all_headers,
+                    allMimeTypes: options.all_mimetypes,
+                    allQueryArgs: options.all_queryargs,
+                    allPostParams: options.all_postparams,
+                });
+                console.log('Sanitization completed');
+            } catch (e) {
+                console.error('Error during sanitization:', e);
+                // Handle the error appropriately
+            }
 
-                    console.log(payload);
-                    const jobId = await queue.push(payload);
+            // Parse the string back into a JSON object
+            let scrubbedHar = JSON.parse(scrubbedHarString);
 
-                    // TODO - looks like when an attachment comes in on an issue created event, we handle it correctly via the standard process
-                    // however, we need to update the description (and maybe other fields that can include media?) similar to what we're doing
-                    // with comments. The problem is that the attachment happens in it's own event, so we would need to ensure we go through and 
-                    // look for description and other fields to update only once the attachment replacing is completed. 
-                    
-                    // TODO P1 - this logic isn't right. We need to persist the dedupe long enough so that when Atlassian sends dupe events, we can reject them for a while. 
-                    // Deleting the dedpue id here doesn't help. Instead we should probably be storing a timestamp, then searching for all keys with a timestamp 
-                    // that is at least some amount of time in the past and then cleaning those up
-                    const dedupeId = issueIdOrKey + fileName + attachmentId;
-                    await storage.delete(hash(dedupeId));
-                    return Promise.resolve({
-                        statusCode: 200 
-                    });
+            // Create a new attachment with the sanitized content
+            const createAttachmentSuccessful = await createAttachment(issueIdOrKey, scrubbedHar, fileName);
+            // Delete the original attachments only if the new attachment was created successfully
+            if (createAttachmentSuccessful.status) {
+
+                // Dispatch comment cleanup to seperate queue
+                const payload = {
+                    attachmentId: attachmentId,
+                    issueIdOrKey: issueIdOrKey,
+                    originalAttachmentMediaId: originalAttachmentMediaId,
+                    newAttachmentMediaId: createAttachmentSuccessful.id,
+                };
+
+                console.log(payload);
+                const jobId = await queue.push(payload);
+
+                // TODO - looks like when an attachment comes in on an issue created event, we handle it correctly via the standard process
+                // however, we need to update the description (and maybe other fields that can include media?) similar to what we're doing
+                // with comments. The problem is that the attachment happens in it's own event, so we would need to ensure we go through and 
+                // look for description and other fields to update only once the attachment replacing is completed. 
+                
+                // TODO P1 - this logic isn't right. We need to persist the dedupe long enough so that when Atlassian sends dupe events, we can reject them for a while. 
+                // Deleting the dedpue id here doesn't help. Instead we should probably be storing a timestamp, then searching for all keys with a timestamp 
+                // that is at least some amount of time in the past and then cleaning those up
+                const dedupeId = issueIdOrKey + fileName + attachmentId;
+                await storage.delete(hash(dedupeId));
+                return Promise.resolve({
+                    statusCode: 200 
+                });
 
                 }
-            } else {
-                // This happens when something fails on the Cloudflare side of things
-                // Docs at https://developer.atlassian.com/platform/forge/runtime-reference/async-events-api/#retry-events
-
-                console.error('Error from server:', responseText);
-
-                return new InvocationError({
-
-                    // The App can request the retry to happen after a certain time period elapses
-            
-                    retryAfter: 60, // seconds
-            
-                    // The App should provide a reason as to why they are retrying.
-            
-                    retryReason: InvocationErrorCode.FUNCTION_RETRY_REQUEST
-                });
-            }
         } catch (error) {
             console.error('Error:', error);
         }
