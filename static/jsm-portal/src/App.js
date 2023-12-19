6fc8543701e647933b2c9c70913ebe02fb544c17
bberenberg <bberenberg@gmail.com>
2023-12-19 09:05:49 +0100

    UX improvements

--- static/jsm-portal/src/App.js
+++ static/jsm-portal/src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { invoke } from '@forge/bridge';
-import { DropzoneArea } from 'material-ui-dropzone';
+import { useDropzone } from 'react-dropzone';
 import Button from '@atlaskit/button';
 import Page from '@atlaskit/page';
 import { sanitizeHar } from 'har-cleaner';
 
 function HarFileScrubber() {
     const [settings, setSettings] = useState({});
-    const [harFile, setHarFile] = useState(null);
-    const [scrubbedHarFile, setScrubbedHarFile] = useState(null);
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+    const [scrubbedFiles, setScrubbedFiles] = useState([]);
     const [isProcessing, setIsProcessing] = useState(false);
 
     useEffect(() => {
@@ -20,61 +20,74 @@ function HarFileScrubber() {
         fetchSettings();
     }, []);
 
-    const handleFileUpload = (files) => {
-        const file = files[0];
-        setHarFile(file);
-    };
-
-    const scrubHarFile = async () => {
-        if (!harFile) return;
-
+    const onDrop = async (acceptedFiles) => {
+        setUploadedFiles(acceptedFiles);
         setIsProcessing(true);
 
         try {
-            const harContent = await harFile.text();
-            const harObject = JSON.parse(harContent);
+            const scrubbedFiles = await Promise.all(acceptedFiles.map(async (file) => {
+                const harContent = await file.text();
+                const harObject = JSON.parse(harContent);
+                const scrubbedHarContent = sanitizeHar(harObject, settings);
+                const scrubbedBlob = new Blob([JSON.stringify(scrubbedHarContent, null, 2)], { type: 'application/json' });
+                return { blob: scrubbedBlob, name: file.name.replace(/\.har$/, '-cleaned.har') };
+            }));
 
-            // Apply the sanitizeHar function
-            const scrubbedHarContent = sanitizeHar(harObject, settings);
-            const scrubbedBlob = new Blob([JSON.stringify(scrubbedHarContent)], { type: 'application/json' });
-            setScrubbedHarFile(scrubbedBlob);
+            setScrubbedFiles(scrubbedFiles);
         } catch (error) {
-            console.error('Error scrubbing HAR file:', error);
-            // Handle the error appropriately
+            console.error('Error scrubbing HAR files:', error);
         } finally {
             setIsProcessing(false);
         }
     };
 
-    const downloadScrubbedFile = () => {
-        if (!scrubbedHarFile) return;
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ 
+        onDrop, 
+        accept: '.har', 
+        maxSize: 75 * 1024 * 1024,
+        multiple: true
+    });
 
-        const url = window.URL.createObjectURL(scrubbedHarFile);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'scrubbed.har');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+    const resetScrubber = () => {
+        setUploadedFiles([]);
+        setScrubbedFiles([]);
+        setIsProcessing(false);
     };
 
     return (
         <Page>
             <div style={{ maxWidth: '800px', margin: '0 auto' }}>
-                <DropzoneArea
-                    onChange={handleFileUpload}
-                    acceptedFiles={['application/json', '.har']}
-                    dropzoneText="Drag and drop a HAR file here or click"
-                    filesLimit={1}
-                    maxFileSize={75 * 1024 * 1024}
-                />
-                <Button onClick={scrubHarFile} appearance="primary" isDisabled={isProcessing}>
-                    {isProcessing ? 'Processing...' : 'Scrub HAR File'}
-                </Button>
-                {scrubbedHarFile && (
-                    <Button onClick={downloadScrubbedFile}>
-                        Download Scrubbed File
-                    </Button>
+                {scrubbedFiles.length === 0 && (
+                    <div {...getRootProps()} style={{ border: '2px dashed #ccc', padding: '20px', textAlign: 'center' }}>
+                        <input {...getInputProps()} />
+                        {isDragActive ? 
+                            <p>Drop the HAR files here...</p> : 
+                            <p>If you need to provide HAR files, drag 'n' drop them here, or click to select files to remove sensitive data before attaching them to the request.</p>
+                        }
+                    </div>
+                )}
+                {isProcessing && <p>Processing...</p>}
+                {scrubbedFiles.length > 0 && (
+                    <div>
+                        {scrubbedFiles.map((file, index) => (
+                            <div key={index}>
+                                <Button onClick={() => {
+                                    const url = window.URL.createObjectURL(file.blob);
+                                    const link = document.createElement('a');
+                                    link.href = url;
+                                    link.setAttribute('download', file.name);
+                                    document.body.appendChild(link);
+                                    link.click();
+                                    document.body.removeChild(link);
+                                }}>
+                                    Download {file.name}
+                                </Button>
+                            </div>
+                        ))}
+                        <Button onClick={resetScrubber} appearance="warning">
+                            Process New Files
+                        </Button>
+                    </div>
                 )}
             </div>
         </Page>
