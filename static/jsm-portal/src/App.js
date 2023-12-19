93acdbc896b9e66c82170ce3e59cfabff69f2b24
bberenberg <bberenberg@gmail.com>
2023-12-19 10:32:56 +0100

    more ux fixes

--- static/jsm-portal/src/App.js
+++ static/jsm-portal/src/App.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { invoke } from '@forge/bridge';
 import { useDropzone } from 'react-dropzone';
-import Button from '@atlaskit/button';
-import Page from '@atlaskit/page';
+import JSZip from 'jszip';
 import { sanitizeHar } from 'har-cleaner';
+import DropdownMenu, { DropdownItem, DropdownItemGroup } from '@atlaskit/dropdown-menu';
+import ChevronDownIcon from '@atlaskit/icon/glyph/chevron-down';
+import Button, { IconButton, SplitButton } from '@atlaskit/button/new';
+import Spinner from '@atlaskit/spinner';
+import Page from '@atlaskit/page';
 
 function HarFileScrubber() {
     const [settings, setSettings] = useState({});
     const [uploadedFiles, setUploadedFiles] = useState([]);
     const [scrubbedFiles, setScrubbedFiles] = useState([]);
     const [isProcessing, setIsProcessing] = useState(false);
+    const [processingCompleted, setProcessingCompleted] = useState(false);
 
     useEffect(() => {
-        // Fetch settings
         const fetchSettings = async () => {
             const loadedSettings = await invoke('getSettings');
             setSettings(loadedSettings);
@@ -23,22 +27,26 @@ function HarFileScrubber() {
     const onDrop = async (acceptedFiles) => {
         setUploadedFiles(acceptedFiles);
         setIsProcessing(true);
+        setProcessingCompleted(false);
 
-        try {
-            const scrubbedFiles = await Promise.all(acceptedFiles.map(async (file) => {
-                const harContent = await file.text();
-                const harObject = JSON.parse(harContent);
-                const scrubbedHarContent = sanitizeHar(harObject, settings);
-                const scrubbedBlob = new Blob([JSON.stringify(scrubbedHarContent, null, 2)], { type: 'application/json' });
-                return { blob: scrubbedBlob, name: file.name.replace(/\.har$/, '-cleaned.har') };
-            }));
+        setTimeout(async () => {
+            try {
+                const scrubbedFiles = await Promise.all(acceptedFiles.map(async (file) => {
+                    const harContent = await file.text();
+                    const harObject = JSON.parse(harContent);
+                    const scrubbedHarContent = sanitizeHar(harObject, settings);
+                    const scrubbedBlob = new Blob([JSON.stringify(scrubbedHarContent, null, 2)], { type: 'application/json' });
+                    return { blob: scrubbedBlob, name: file.name.replace(/\.har$/, '-cleaned.har') };
+                }));
 
-            setScrubbedFiles(scrubbedFiles);
-        } catch (error) {
-            console.error('Error scrubbing HAR files:', error);
-        } finally {
-            setIsProcessing(false);
-        }
+                setScrubbedFiles(scrubbedFiles);
+                setProcessingCompleted(true);
+            } catch (error) {
+                console.error('Error scrubbing HAR files:', error);
+            } finally {
+                setIsProcessing(false);
+            }
+        }, 1000);
     };
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ 
@@ -52,40 +60,70 @@ function HarFileScrubber() {
         setUploadedFiles([]);
         setScrubbedFiles([]);
         setIsProcessing(false);
+        setProcessingCompleted(false);
+    };
+
+    const downloadAllFiles = async () => {
+        const zip = new JSZip();
+        scrubbedFiles.forEach(file => {
+            zip.file(file.name, file.blob);
+        });
+
+        const zipBlob = await zip.generateAsync({ type: 'blob' });
+        const url = window.URL.createObjectURL(zipBlob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'scrubbed_files.zip');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     };
 
     return (
         <Page>
-            <div style={{ maxWidth: '800px', margin: '0 auto' }}>
+            <div style={{ maxWidth: '800px', margin: '0 auto', textAlign: 'center' }}>
                 {scrubbedFiles.length === 0 && (
                     <div {...getRootProps()} style={{ border: '2px dashed #ccc', padding: '20px', textAlign: 'center' }}>
-                        <input {...getInputProps()} />
-                        {isDragActive ? 
-                            <p>Drop the HAR files here...</p> : 
-                            <p>If you need to provide HAR files, drag 'n' drop them here, or click to select files to remove sensitive data before attaching them to the request.</p>
-                        }
-                    </div>
+                    <input {...getInputProps()} />
+                    {isDragActive ? 
+                        <p>Drop the HAR files here...</p> : 
+                        <p>If you need to attach HAR files, drag 'n' drop the files here first. They will have sensitive data removed without leaving your browser. Then upload the resulting files to the attachment field above.</p>
+                    }
+                </div>
                 )}
-                {isProcessing && <p>Processing...</p>}
+                {isProcessing && <Spinner size="large" />}
+                {processingCompleted && <p>Cleaning completed, yes it's that fast.</p>}
                 {scrubbedFiles.length > 0 && (
-                    <div>
-                        {scrubbedFiles.map((file, index) => (
-                            <div key={index}>
-                                <Button onClick={() => {
-                                    const url = window.URL.createObjectURL(file.blob);
-                                    const link = document.createElement('a');
-                                    link.href = url;
-                                    link.setAttribute('download', file.name);
-                                    document.body.appendChild(link);
-                                    link.click();
-                                    document.body.removeChild(link);
-                                }}>
-                                    Download {file.name}
-                                </Button>
-                            </div>
-                        ))}
-                        <Button onClick={resetScrubber} appearance="warning">
-                            Process New Files
+                    <div style={{ marginTop: '20px' }}>
+                        <Button appearance="primary" onClick={downloadAllFiles} style={{ marginRight: '8px' }}>
+                            Download All
+                        </Button>
+                        <DropdownMenu
+                            triggerButtonProps={{
+                                iconAfter: <ChevronDownIcon label="More options" />,
+                                appearance: 'primary',
+                                isSelected: false
+                            }}
+                            triggerType="button"
+                        >
+                            <DropdownItemGroup title="Individual Files">
+                                {scrubbedFiles.map((file, index) => (
+                                    <DropdownItem key={index} onClick={() => {
+                                        const url = window.URL.createObjectURL(file.blob);
+                                        const link = document.createElement('a');
+                                        link.href = url;
+                                        link.setAttribute('download', file.name);
+                                        document.body.appendChild(link);
+                                        link.click();
+                                        document.body.removeChild(link);
+                                    }}>
+                                        {file.name}
+                                    </DropdownItem>
+                                ))}
+                            </DropdownItemGroup>
+                        </DropdownMenu>
+                        <Button onClick={resetScrubber} appearance="subtle" style={{ marginLeft: '10px' }}>
+                            Reset
                         </Button>
                     </div>
                 )}
